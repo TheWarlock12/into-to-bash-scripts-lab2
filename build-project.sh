b50d33f6efaa5cf6476faac5b00340fe151174a8
MicahE <micah.eastwood@gmail.com>
2025-09-25 15:38:21 -0600

    initial commit

--- build-project.sh
+++ build-project.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+name=$1
+#Gives the name of tthe directory
+
+mkdir $name
+cd $name
+touch "README.md"
+touch $name.sh
+#File Creation
+
+#giver permissions executable
+chmod +x build-project.sh
+ls -1 build-project.sh
+
+
+#Initialize the Git repo
+echo "initializing new Git repository..."
+git init
+git add .
+git commit -m "Initial Commit"
+git push
+
